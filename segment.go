47930208115526376c65cdbb90d2bf87317981b7
Marty Schoch <marty.schoch@gmail.com>
2020-12-18 12:29:58 -0500

    fix import

--- segment.go
+++ segment.go
@@ -18,7 +18,7 @@ import (
 	"fmt"
 
 	"github.com/RoaringBitmap/roaring"
-	"github.com/blevesearch/bleve_index_api"
+	index "github.com/blevesearch/bleve_index_api"
 	"github.com/couchbase/vellum"
 )
 
