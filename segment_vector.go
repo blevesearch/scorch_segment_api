58b9d7f80a499c8b9c79d6bd4f2cd59d0ed17420
Thejas-bhat <thejas.orkombu@couchbase.com>
2023-08-09 12:19:26 +0530

    added build tags

--- segment_vector.go
+++ segment_vector.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build densevector
+// +build densevector
+
 package segment
 
 import "github.com/RoaringBitmap/roaring"
