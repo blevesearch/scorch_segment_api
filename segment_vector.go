eef141808214cc7da7642aae174a7834175e6bee
Abhinav Dangeti <abhinav@couchbase.com>
2023-09-15 17:19:10 -0600

    Update build tag to `vectors`

--- segment_vector.go
+++ segment_vector.go
@@ -1,4 +1,4 @@
-//	Copyright (c) 2023 Couchbase, Inc.
+//  Copyright (c) 2023 Couchbase, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build densevector
-// +build densevector
+//go:build vectors
+// +build vectors
 
 package segment
 
